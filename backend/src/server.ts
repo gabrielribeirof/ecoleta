8778087ca82708d6e32a5fe567f35fb98fbc0a66
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-05 04:15:17 -0300

    feat(server): implements dotenv process

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
+import 'dotenv/config';
+
 import express from 'express';
-import routes from './routes';
 import path from 'path';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333, () => console.log('Server running'));
+app.listen(process.env.APP_PORT, () => console.log('Server running'));
