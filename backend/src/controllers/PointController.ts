24aa89c225b86c00c6bd9f5b3be7be095ca1f58c
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-18 11:04:12 -0300

    feat(PointController): improve all methods

--- backend/src/controllers/PointController.ts
+++ backend/src/controllers/PointController.ts
@@ -2,10 +2,21 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointController {
-  async index(request: Request, res: Response) {
-    const points = await knex('points').select('*');
+  async index(request: Request, response: Response) {
+    const { city, state, items } = request.query;
 
-    return res.json(points);
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('state', String(state))
+      .distinct('points.*');
+
+    return response.json(points);
   }
 
   async show(request: Request, response: Response) {
@@ -18,9 +29,9 @@ class PointController {
     }
 
     const items = await knex('items')
-    .join('point_items', 'items.id', '=', 'point_items.item_id')
-    .where('point_items.point_id', id)
-    .select('items.title');
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
 
     return response.json({ point, items });
   }
@@ -41,7 +52,7 @@ class PointController {
 
     const point = {
       name,
-      image: 'image-fake',
+      image: request.file.filename,
       email,
       whatsapp,
       city,
@@ -54,12 +65,15 @@ class PointController {
     
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: Number) => {
-      return {
-        item_id,
-        point_id
-      }
-    });
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
 
     await trx('point_items').insert(pointItems);
 
