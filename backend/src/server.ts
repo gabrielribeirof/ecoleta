2dbfacf9e391d85ab9769ab3b4834b7422f53938
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-18 11:06:45 -0300

    feat(server): use routes file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,12 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(process.env.APP_PORT, () => console.log('Server running'));
