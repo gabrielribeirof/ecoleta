5d0b12b17d4e8f10371e020a57849e49ccfc54e2
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-19 17:18:48 -0300

    feat(server): implement cors and celebrate

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,19 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(process.env.APP_PORT, () => console.log('Server running'));
