b45dd13c32302b94ab7bcfd93c87cb963aa052a6
ogabrielribeirof <ogabrielribeirof@gmail.com>
2020-06-25 09:38:26 -0300

    feat(seeds): type functions

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -1,12 +1,12 @@
 import Knex from 'knex';
 
-export async function seed(knex: Knex) {
+export async function seed(knex: Knex): Promise<Knex.QueryBuilder> {
   await knex('items').insert([
     { title: 'Lâmpadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
     { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
     { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
     { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-    { title: 'Óleo de Cozinha', image: 'oleo.svg' }
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
   ]);
 }
